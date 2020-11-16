988a99dc3d617c7fbf65500e394b7c2664352e63
minato9212 <56445304+minato9212@users.noreply.github.com>
2020-11-16 14:32:07 +0900

    Add files via upload

---
+++ TempoAnalyzer.js
@@ -0,0 +1,30 @@
+'usestrict';
+
+const TempoAnalyzer = class{
+	constructor(object){
+		window.AudioContext = window.AudioContext || window.webkitAudioContext();
+		this.context = new AudioContext();
+		if(object.path){
+			const extension = object.path.split('.').slice(-1)[0];
+			if(extension.indexod('mp3') < 0) throw new Error(`${extension} is not suported.`);
+			fetch(object.path).then(response=>{
+				response.arrayBuffer();
+			}).then(data=>{
+				this.audioData = ctx.decodeAudioData(data);
+				this.onloaded();
+			}).then(error=>{
+				throw new Error(error);
+			});
+		}else if(object.element){
+			this.audioData = ctx.createMediaElementSource(object.element);
+			this.onloaded();
+		}else{
+			throw new Error('Error: No element.');
+		}
+	}
+	loaded(){
+		
+	}
+}
+
+export TempoAnalyzer;
---
+++ main.js
@@ -0,0 +1,8 @@
+import TempoAnalyzer from './include/TempoAnalyzer.js';
+
+const init = ()=>{
+	console.log('init');
+	const analyzer = new TempoAnalyzer();
+}
+
+document.addEventListener('DOMContentLoaded', init);
